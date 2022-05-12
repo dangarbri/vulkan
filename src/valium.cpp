86e99557fb88c5f7119fce01244353b9dab55c4c
Daniel Garcia-Briseno <daniel.garcia.briseno@appsolutelywonderful.com>
2022-05-12 16:29:06 -0400

    Create valium_device for wrapping logical device creation

--- src/valium.cpp
+++ src/valium.cpp
@@ -9,32 +9,44 @@
 #include "valium.h"
 #include "valium_queue.h"
 #include "validation_layers.h"
+#include "valium_device.h"
 
 struct Valium::impl {
   /** Vulkan instance for use with vulkan APIs */
   VkInstance instance;
-  /** Holds the physical device that was selected. */
-  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
   /** Self */
   Valium* inst;
+  /** Interface for interacting with the GPU. Set by @a selectDevice() */
+  ValiumDevice* device = nullptr;
+  /** Extensions to be requested on the instance */
+  std::vector<const char*> requestedExtensions;
 
   /** Creates the vulkan instance and assigns it to instance */
   void initVulkanInstance(const char* app_name);
 
-  /**
-   * Gets the required glfw extensions to pass to vulkan and adds
-   * them to the given info struct.
-   */
-  void setGlfwExtensions(VkInstanceCreateInfo* info);
-
   /** Checks the vulkan API for a list of available extensions */
   std::vector<VkExtensionProperties> getVulkanExtensions();
+
+  /**
+   * @brief Applies instance extensions.
+   * This function will set VK_KHR_get_physical_device_properties2 if it is
+   * available since it is a dependency of VK_KHR_portability_subset. And
+   * VK_KHR_portability_subset MUST be set (per the spec) if it is an available
+   * extension, therefore VK_KHR_get_physical_device_properties2 must also be set.
+   *
+   * This function also sets the extensions required by GLFW
+   *
+   * This function may be extended to include other extensions.
+   *
+   * @param[out] info CreateInfo data to place extensions into.
+   */
+  void SetInstanceExtensions(VkInstanceCreateInfo &info);
 
   /** Verifies that the glfw extensions exist in vulkan's available extensions */
   bool verifyGlfwWorksWithVulkan();
 
   /** Selects a GPU to use for rendering */
-  void selectPhysicalDevice();
+  void selectDevice();
 
   /** Checks if a GPU is suitable for rendering */
   bool isDeviceSuitable(VkPhysicalDevice device);
@@ -44,12 +56,16 @@ Valium::Valium(const char* app_name) {
   _impl = new impl();
   _impl->inst = this;
   _impl->initVulkanInstance(app_name);
-  _impl->selectPhysicalDevice();
+  _impl->selectDevice();
 }
 
 Valium::~Valium() {
-  std::cout << "Destroyed vulkan instance" << std::endl;
+  if (_impl->device != nullptr) {
+    delete _impl->device;
+  }
+  
   vkDestroyInstance(_impl->instance, nullptr);
+  std::cout << "Destroyed vulkan instance" << std::endl;
 
   delete _impl;
 }
@@ -83,8 +99,8 @@ void Valium::impl::initVulkanInstance(const char* app_name) {
   ValidationLayers::enableValidationLayers(&createInfo);
 #endif
 
-  // Load GLFW extensions into the info struct
-  setGlfwExtensions(&createInfo);
+  // Update createInfo with desired extensions
+  SetInstanceExtensions(createInfo);
 
   if (verifyGlfwWorksWithVulkan() == false) {
      throw std::runtime_error("This Vulkan API does not support GLFW");
@@ -94,16 +110,6 @@ void Valium::impl::initVulkanInstance(const char* app_name) {
   }
  }
 
-void Valium::impl::setGlfwExtensions(VkInstanceCreateInfo* info) {
-  uint32_t glfwExtensionCount = 0;
-  const char** glfwExtensions;
-
-  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-  info->enabledExtensionCount = glfwExtensionCount;
-  info->ppEnabledExtensionNames = glfwExtensions;
-}
-
 std::vector<VkExtensionProperties> Valium::impl::getVulkanExtensions() {
   uint32_t extensionCount = 0;
   vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
@@ -111,6 +117,27 @@ std::vector<VkExtensionProperties> Valium::impl::getVulkanExtensions() {
   std::vector<VkExtensionProperties> extensions(extensionCount);
   vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
   return extensions;
+}
+
+void Valium::impl::SetInstanceExtensions(VkInstanceCreateInfo &info) {
+  // Get the available extensions
+  std::vector<std::string> availableExtensions = inst->GetAvailableExtensions();
+  // Check if the available extensions contains VK_KHR_get_physical_device_properties2
+  for (auto ext : availableExtensions) {
+    if (ext == "VK_KHR_get_physical_device_properties2") {
+      requestedExtensions.push_back("VK_KHR_get_physical_device_properties2");
+    }
+  }
+
+  // Push the GLFW extensions into the list
+  uint32_t glfwExtensionCount;
+  const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+  for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+    requestedExtensions.push_back(glfwExtensions[i]);
+  }
+
+  info.enabledExtensionCount = requestedExtensions.size();
+  info.ppEnabledExtensionNames = requestedExtensions.data();
 }
 
 bool Valium::impl::verifyGlfwWorksWithVulkan() {
@@ -136,7 +163,7 @@ bool Valium::impl::verifyGlfwWorksWithVulkan() {
   return true;
 }
 
-void Valium::impl::selectPhysicalDevice() {
+void Valium::impl::selectDevice() {
   uint32_t deviceCount = 0;
   vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 
@@ -147,22 +174,27 @@ void Valium::impl::selectPhysicalDevice() {
   std::vector<VkPhysicalDevice> devices(deviceCount);
   vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
+  VkPhysicalDevice selectedDevice = VK_NULL_HANDLE;
+
   // TODO: Rate installed devices and select the one with the most
   // appropriate features
   for (VkPhysicalDevice device : devices) {
     if (isDeviceSuitable(device)) {
-      if (physicalDevice == VK_NULL_HANDLE) {
+      if (selectedDevice == VK_NULL_HANDLE) {
 #ifndef NDEBUG
         std::cout << "Choosing device " << device << std::endl;
 #endif
-        physicalDevice = device;
+        selectedDevice = device;
       }
     }
   }
 
-  if (physicalDevice == VK_NULL_HANDLE) {
+  if (selectedDevice == VK_NULL_HANDLE) {
     throw std::runtime_error("failed to find a suitable GPU!");
   }
+
+  // Now that a device has been selected, wrap it with some valium.
+  device = new ValiumDevice(selectedDevice);
 }
 
 bool Valium::impl::isDeviceSuitable(VkPhysicalDevice device) {
--- src/valium_device.cpp
+++ src/valium_device.cpp
@@ -1,28 +1,50 @@
 #include "valium_device.h"
 #include "valium_queue.h"
+#include "validation_layers.h"
+#include <vector>
+#include <iostream>
+#include <string>
 
 struct ValiumDevice::ValiumDeviceImpl {
   /** Represents the physical device for this @a ValiumDevice */
-  const VkPhysicalDevice device;
+  const VkPhysicalDevice physicalDevice;
+
+  /** Logical Device to be used with the vulkan API */
+  VkDevice device;
+
+  /** Queue descriptor for interfacing with the GPU's command queue */
+  VkQueue graphicsQueue;
+
+  /** Extensions to enable on the device */
+  std::vector<const char*> desiredExtensions;
 
   /** Constructs and assigns the constant device */
-  ValiumDeviceImpl(const VkPhysicalDevice d) : device(d) {}
+  ValiumDeviceImpl(const VkPhysicalDevice d) : physicalDevice(d) {}
 
   /** Creates the logical device around @a device */
   void CreateLogicalDevice();
+
+  /** Sets the extensions to be used when creating the logical device with CreateLogicalDevice()
+   * @param[out] createInfo The struct to place the extensions into.
+   */
+  void SetExtensions(VkDeviceCreateInfo& createInfo);
 };
 
-ValiumDevice::ValiumDevice(const VkPhysicalDevice device) {
-  _impl = new ValiumDeviceImpl(device);
+ValiumDevice::ValiumDevice(const VkPhysicalDevice physicalDevice) {
+  _impl = new ValiumDeviceImpl(physicalDevice);
   _impl->CreateLogicalDevice();
+#ifndef NDEBUG
+  std::cout << "Created logical device" << std::endl;
+#endif
 }
 
 ValiumDevice::~ValiumDevice() {
+  vkDestroyDevice(_impl->device, nullptr);
   delete _impl;
 }
 
 void ValiumDevice::ValiumDeviceImpl::CreateLogicalDevice() {
-  QueueFamilyIndices indices = ValiumQueue::GetQueueIndices(device);
+  QueueFamilyIndices indices = ValiumQueue::GetQueueIndices(physicalDevice);
 
   VkDeviceQueueCreateInfo queueCreateInfo{};
   // Specify that we would like to use the graphics queue
@@ -41,4 +63,47 @@ void ValiumDevice::ValiumDeviceImpl::CreateLogicalDevice() {
   createInfo.pQueueCreateInfos = &queueCreateInfo;
   createInfo.queueCreateInfoCount = 1;
   createInfo.pEnabledFeatures = &deviceFeatures;
+
+#ifndef NDEBUG
+  createInfo.enabledLayerCount = ValidationLayers::validationLayers.size();
+  createInfo.ppEnabledLayerNames = ValidationLayers::validationLayers.data();
+#else
+  createInfo.enabledLayerCount = 0;
+#endif
+
+  SetExtensions(createInfo);
+
+  auto result = vkCreateDevice(physicalDevice, &createInfo, nullptr, &device);
+  if (result != VK_SUCCESS) {
+    std::cout << "VkCreateDevice: " << result << std::endl;
+    throw std::runtime_error("failed to create logical device!");
+  }
+
+  vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+}
+
+void ValiumDevice::ValiumDeviceImpl::SetExtensions(VkDeviceCreateInfo &createInfo) {
+  uint32_t numExtensions;
+  vkEnumerateDeviceExtensionProperties(physicalDevice, nullptr, &numExtensions, nullptr);
+
+  std::vector<VkExtensionProperties> properties(numExtensions);
+  vkEnumerateDeviceExtensionProperties(physicalDevice, nullptr, &numExtensions, properties.data());
+
+  std::cout << "Found properties: " << std::endl;
+  for (VkExtensionProperties props : properties) {
+    std::cout << "\t" << props.extensionName << std::endl;
+    if (std::string(props.extensionName) == "VK_KHR_portability_subset") {
+      desiredExtensions.push_back("VK_KHR_portability_subset");
+      // This extension is required by portability_subset per the documentation found here:
+      // https://vulkan.lunarg.com/doc/view/1.3.211.0/mac/1.3-extensions/vkspec.html#VK_KHR_portability_subset
+      std::cout << "\t\t" << "Adding " << props.extensionName << " to desired extension list" << std::endl;
+    }
+  }
+
+  std::cout << "Requested extensions: " << std::endl;
+  for (auto ext : desiredExtensions) {
+    std::cout << "\t" << ext << std::endl;
+  }
+  createInfo.enabledExtensionCount = desiredExtensions.size();
+  createInfo.ppEnabledExtensionNames = desiredExtensions.data();
 }
